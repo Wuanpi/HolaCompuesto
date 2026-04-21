db7c52513f516c0a2c920090336f543e4f91b962
Wuanpi <aprendiz.senadev@ienm.com.co>
2026-04-21 11:16:33 -0500

    Se instalan dependencias dentro de un entorno virtual Venv

--- hola_compuesto.py
+++ hola_compuesto.py
@@ -1,26 +1,6 @@
-import flet as ft
+from fastapi import FastAPI
+app = FastAPI()
 
-def main(page: ft.Page):
-    page.title = "Contador de Saludos"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
-    input = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
-
-    def minus_click(e):
-        input.value = str(int(input.value) - 1)
-
-    def plus_click(e):
-        input.value = str(int(input.value) + 1)
-
-    page.add(
-        ft.Row(
-            alignment=ft.MainAxisAlignment.CENTER,
-            controls=[
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
-                input,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
-            ],
-        )
-    )
-
-ft.run(main)
+@app.get("/")
+def read_root():
+    return {"mensaje": "Hola Mundo"}
